9ba5a9310324f3b671baca4ef02988579e12331e
Sanzida Akhter Anee <168485904+sanzidaanee@users.noreply.github.com>
2025-02-08 06:36:52 +0600

    Add files via upload

---
+++ Stage 0/Code/stage 0_task.R
@@ -0,0 +1,25 @@
+#Task
+
+In stage 0 the task is to organize the following information of the team members (names, slack names, emails, hobbies, countries, discipline, and preferred programming languages) using a simple data structure using R or Python.
+
+
+
+##Assign variale for each information
+
+
+first <- c('Bezaleel', 'Kazadi', 'Hana', 'Onyinye', 'Sanzida')
+last <- c('Akinbami', 'Ngole', 'Nadir', 'Maryrose_Ugwu', 'Akhter_Anee')
+slack_name <- c('@Bez', '@Tatiene', '@Hana', '@Onyinye', '@Sanzida')
+email <- c('bezaleelakinbami16@gmail.com', 'tatianagoie17@gmail.com', 'hananadir04@gmail.com', 'moyinye80@gmail.com', 'aneesanzidaakhter@gmail.com')
+hobby <- c('gaming', 'gaming', 'Reading', 'Traveling', 'Blogging')
+country <- c('Nigeria', 'South Africa', 'Sudan', 'Nigeria', 'Bangladesh')
+discipline <- c('Bioinformatics', 'Bioinformatics', 'Medicinal chemistry', 'Zoology', 'Bioinformatics')
+program_lang <- c('R', 'Python', 'Python', 'Python', 'Python')
+
+
+
+## Creating a data frame with team members' information
+
+
+team_data<- data.frame(First = first, Last = last, Slack_Name = slack_name, Email = email, Hobby = hobby, Country = country, Discipline = discipline, Program_Lang = program_lang)
+team_data
